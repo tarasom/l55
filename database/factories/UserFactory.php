df49b7a98e321dc375d3737f676b58515a0d9ff0
Taras Omelianchuk <taras@toavalon.com>
2017-09-14 17:58:46 +0300

    Added auth scaffolding; Added todos

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -23,3 +23,10 @@ $factory->define(App\User::class, function (Faker $faker) {
         'remember_token' => str_random(10),
     ];
 });
+
+$factory->define(App\Todo::class, function (Faker $faker) {
+    return [
+        'task' => $faker->sentence,
+        'done' => rand(0,1),
+    ];
+});
